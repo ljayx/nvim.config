68df0cc0445cb808bc8fc5cb710fa98b2dbdf08a
ljay <microlj@126.com>
2024-08-01 02:16:20 +0000

    fix cpp endless notice

--- init.lua
+++ init.lua
@@ -17,3 +17,8 @@ end
 
 require "lazy_setup"
 require "polish"
+
+-- fix cpp endless notice
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require('lspconfig').clangd.setup({capabilities = capabilities})
